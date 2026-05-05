5b5e913c19bff8f463dec656298afedcb80feb2a
Sean Downes <irishpropertygroupsd@gmail.com>
2026-05-04 21:42:58 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Liam Tennis App",
@@ -11,9 +12,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
